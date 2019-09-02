abf6f9fe0a3398794462eec0976acf06abb7f8b8
Glenn Meering <glenn.dev@hotmail.com>
2019-09-02 12:27:55 +0200

    finished us-1 almost

--- includes/controllers/createTicketController.php
+++ includes/controllers/createTicketController.php
@@ -28,4 +28,25 @@ if ($_POST['type'] === 'ticketcustomerdetails') {
         header("location: ../../createTicket.php?err=$err");
         exit;
     }
+
+    if (!ctype_digit($phoneNumber) || strlen($phoneNumber) != 10) {
+        $err = "Telefoon nummer is niet correct.";
+        header("location: ../../createTicket.php?err=$err");
+        exit;
+    }
+
+    $sql = "INSERT INTO `customers`(`id`, `first`, `middel`, `last`, `email`, `phone`, `business`) VALUES (:id, :first, :middel, :last, :email, :phone, :business)";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+       'id'        => null,
+       'first'     => $firstName,
+       'middel'    => $middelName,
+       'last'      => $lastName,
+       'email'     => $email,
+       'phone'     => $phoneNumber,
+       'business'  => $businessName
+    ]);
+    $succ = "Klant gegevens opgeslagen";
+    header("location: ../../index.php?succ=$succ");
+    exit;
 }
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-require __DIR__ . 'menus/header.php';
+require __DIR__ . '/menus/header.php';
 ?>
     <main>
-
+        <?php
+            if(isset($_GET['succ'])) {
+                echo "<p>{$_GET['succ']}</p>";
+            }
+        ?>
     </main>
 <?php
 require 'menus/footer.php';
