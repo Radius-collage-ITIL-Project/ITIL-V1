1a74303c4191eba7d05e7467ec464765cb9472c6
Unknown <robinadriaensen9@gmail.com>
2019-09-02 11:09:12 +0200

    employee board

---
+++ employeesBoard.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gebruiker
+ * Date: 2-9-2019
+ * Time: 11:06
+ */
+
---
+++ includes/controllers/employeeListController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gebruiker
+ * Date: 2-9-2019
+ * Time: 11:01
+ */
+
+if ($_POST ['type'] == Employees){
+
+}
