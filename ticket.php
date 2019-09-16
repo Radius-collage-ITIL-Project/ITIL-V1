ba33dde1a75eac33289555e5de56c0d732825b8e
Glenn Meering <glenn.dev@hotmail.com>
2019-09-16 16:09:42 +0200

    print out customer data

--- ticket.php
+++ ticket.php
@@ -9,27 +9,32 @@ $ticketId = $_GET['ticketId'];
 
 $sql = "SELECT t.id as ticketId,
                t.title as title,
-               c.name as category
-               s.*,
+               c.name as category,
+               s.*
         FROM `tickets` as t
         LEFT JOIN customers AS s ON t.customerid = s.id
         LEFT JOIN categories AS c ON t.category = c.id
         LEFT JOIN threats AS th ON t.threat = th.id
-        WHERE t.id = :id";
+        WHERE t.id = :ticketid";
 $prepare = $db->prepare($sql);
 $prepare->execute([
-    'id' => $ticketId
+    'ticketid' => $ticketId
 ]);
 $ticket = $prepare->fetch(2);
 if (!$ticket) {
     header("location: index.php" );
     exit;
 }
+
+$customer = $ticket['first'] . " " . $ticket['middel'] . " " . $ticket['last'];
 ?>
 <main>
     <h2>TICKET <?=$ticketId?></h2>
     <div class="shadow-sm border p-2">
         <h3 class="m-0">Klant Gegevens</h3>
-        <p></p>
+        <p>Naam: <?=$customer?></p>
+        <p>Bedrijf: <?=$ticket['business']?></p>
+        <p>Email: <?=$ticket['email']?></p>
+        <p>Telefoon: <?=$ticket['phone']?></p>
     </div>
 </main>
