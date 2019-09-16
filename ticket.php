cc95520dcd2a464577f6ecfb6826b6ccbb7375f6
Glenn Meering <glenn.dev@hotmail.com>
2019-09-16 09:30:41 +0200

    detailpage

--- ticket.php
+++ ticket.php
@@ -1,29 +1,35 @@
 <?php
 require __DIR__ . '/menus/header.php';
-$ticketId = $_GET['ticketId'];
 
 if (empty($_GET['ticketId'])) {
     header("location: index.php" );
     exit;
 }
-var_dump($_GET);
-die;
+$ticketId = $_GET['ticketId'];
 
-//$sql = "SELECT t.id as ticketId,
-//               t.title as title,
-//               c.name as category
-//        FROM `tickets` AS t
-//        INNER JOIN customers AS s ON t.customerid = s.id
-//        INNER JOIN categories AS c ON t.category = c.id
-//        INNER JOIN threats AS th ON t.threat = th.id
-//        WHERE id = :id";
-//$prepare = $db->prepare($sql);
-//$prepare->execute([
-//    'id' => $ticketId
-//]);
-//$ticket = $prepare->fetch(2);
-//if (!$ticket) {
-//    header("location: index.php" );
-//    exit;
-//}
-var_dump($ticket);
+$sql = "SELECT t.id as ticketId,
+               t.title as title,
+               c.name as category
+               s.*,
+        FROM `tickets` as t
+        LEFT JOIN customers AS s ON t.customerid = s.id
+        LEFT JOIN categories AS c ON t.category = c.id
+        LEFT JOIN threats AS th ON t.threat = th.id
+        WHERE t.id = :id";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    'id' => $ticketId
+]);
+$ticket = $prepare->fetch(2);
+if (!$ticket) {
+    header("location: index.php" );
+    exit;
+}
+?>
+<main>
+    <h2>TICKET <?=$ticketId?></h2>
+    <div class="shadow-sm border p-2">
+        <h3 class="m-0">Klant Gegevens</h3>
+        <p></p>
+    </div>
+</main>
